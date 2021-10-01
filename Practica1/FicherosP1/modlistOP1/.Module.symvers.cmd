bebd9017dd6c1dafc0d04f2b2527b909ac28612b
ggasconn <guillermo.gasconsmr@gmail.com>
2021-10-01 15:49:16 +0200

    Cleaned make files

--- Practica1/FicherosP1/modlistOP1/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/kernel/Escritorio/LIN_2021/Practica1/FicherosP1/modlistOP1/Module.symvers := sed 's/ko$$/o/' /home/kernel/Escritorio/LIN_2021/Practica1/FicherosP1/modlistOP1/modules.order | scripts/mod/modpost -m    -o /home/kernel/Escritorio/LIN_2021/Practica1/FicherosP1/modlistOP1/Module.symvers -e -i Module.symvers   -T -
