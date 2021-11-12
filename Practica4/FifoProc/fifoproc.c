788acc96bbcc56edc38b9ff427a0af5fe6c80d21
ggasconn <guillermo.gasconsmr@gmail.com>
2021-11-12 16:01:32 +0100

    Modifide fifoproc

--- Practica4/FifoProc/fifoproc.c
+++ Practica4/FifoProc/fifoproc.c
@@ -1,18 +1,15 @@
-#include <getopt.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <ctype.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <time.h>
-#include <err.h>
-#include <errno.h>
+
+#include <linux/string.h>
+#include <asm-generic/errno.h>
 #include <linux/kfifo.h>
 #include <linux/module.h>
 #include <linux/semaphore.h>
+#include <linux/uaccess.h>
+#include <linux/proc_fs.h>
+#include <linux/kernel.h>
+ 
+#define MAX_KBUF 64
+#define MAX_CBUFFER_LEN 64
 
 
 struct semaphore sem_mtx;
@@ -22,30 +19,14 @@ int prod_count=0,cons_count=0;
 int nr_prod_waiting=0,nr_cons_waiting=0;
 struct kfifo cbuffer;
 
-void fifoproc_open(bool openRead) {
-/*
-while(condición==false) {
-    nr_waiting++;
-    up(&sem_mtx); // "Libera" el mutex
+static struct proc_dir_entry *proc_entry;
 
-    // Se bloquea en la cola
-    if (down_interruptible(&sem_queue)){
-        down(&sem_mtx);
-        nr_waiting--;
-        up(&sem_mtx);
-        return -EINTR;
-    }
-
-    // "Adquiere" el mutex 
-    if (down_interruptible(&sem_mtx)
-    return -EINTR;
-}
-*/
+static int fifoproc_open(struct inode *inode, struct file *file) {
 
     if(down_interruptible(&sem_mtx))
         return -EINTR;
 
-    if(openRead==1){
+    if(file->f_mode & FMODE_READ){
         cons_count++;
         
         while(prod_count<1) {
@@ -61,7 +42,7 @@ while(condición==false) {
             }
 
             // "Adquiere" el mutex 
-            if (down_interruptible(&sem_mtx)
+            if (down_interruptible(&sem_mtx))
                 return -EINTR;
         }      
     }else{
@@ -80,36 +61,42 @@ while(condición==false) {
             }
 
             // "Adquiere" el mutex 
-            if (down_interruptible(&sem_mtx)
+            if (down_interruptible(&sem_mtx))
                 return -EINTR;
         }
     }
-
     up(&sem_mtx);
+    return 0;
 }
 
-int fifoproc_write(char* buff, int len) {
-/*
- if (nr_waiting>0) {
-/* Despierta a uno de los hilos bloqueados
-*/
-up(&sem_queue);
-nr_waiting--;
-}
- * */
-    
+static ssize_t fifoproc_write(struct file *filp, const char __user *buff, size_t len, loff_t *off) {
 
 	char kbuffer[MAX_KBUF];
 
-    if (len> MAX_CBUFFER_LEN || len> MAX_KBUF) { return Error;}
-    if (copy_from_user(kbuffer,buff,len)) { return Error;}
+    if (len> MAX_CBUFFER_LEN || len> MAX_KBUF) { return -ENOSPC;}
+    if (copy_from_user(kbuffer,buff,len)) { return -EFAULT;}
+
+    kbuffer[len] = '\0';
+    *off+=len;
 
      if(down_interruptible(&sem_mtx))
         return -EINTR;
     /* Esperar hasta que haya hueco para insertar (debe haber consumidores) */
-    
-    while (kfifo_avail(&cbuffer)<len && cons_count>0){
-        cond_wait(prod,mtx);
+    while(kfifo_avail(&cbuffer)<len && cons_count>0) {
+        nr_prod_waiting++;
+        up(&sem_mtx); // "Libera" el mutex
+
+        // Se bloquea en la cola
+        if (down_interruptible(&sem_prod)){
+            down(&sem_mtx);
+            nr_prod_waiting--;
+            up(&sem_mtx);
+            return -EINTR;
+        }
+
+        // "Adquiere" el mutex 
+        if (down_interruptible(&sem_mtx))
+            return -EINTR;
     }
 
     /* Detectar fin de comunicación por error (consumidor cierra FIFO antes) */
@@ -117,46 +104,141 @@ nr_waiting--;
 
     kfifo_in(&cbuffer,kbuffer,len);
     /* Despertar a posible consumidor bloqueado */
-    cond_signal(cons);
+    if (nr_cons_waiting>0) {
+        up(&sem_cons);
+        nr_cons_waiting--;
+    }
     up(&sem_mtx);
     return len;
 }
 
-int fifoproc_read(const char* buff, int len) {
+static ssize_t fifoproc_read(struct file *filp, char __user *buff, size_t len, loff_t *off) {
     char kbuffer[MAX_KBUF];
+    int nr_bytes=0;
+    int bytes_extracted;
+    int val;
 
-    if (len> MAX_CBUFFER_LEN || len> MAX_KBUF) { return Error;}
+    if ((*off)>0)
+        return 0;
+    
+
+    if (len> MAX_CBUFFER_LEN || len> MAX_KBUF) { return -ENOSPC;}
 
      if(down_interruptible(&sem_mtx))
         return -EINTR;
     
-    while (kfifo_len(&cbuffer)<len && prod_count>0){
-        cond_wait(cons,mtx);
+    while(kfifo_len(&cbuffer)<len && prod_count>0) {
+        nr_cons_waiting++;
+        up(&sem_mtx); // "Libera" el mutex
+
+        // Se bloquea en la cola
+        if (down_interruptible(&sem_cons)){
+            down(&sem_mtx);
+            nr_cons_waiting--;
+            up(&sem_mtx);
+            return -EINTR;
+        }
+
+        // "Adquiere" el mutex 
+        if (down_interruptible(&sem_mtx))
+            return -EINTR;
     }
     
     if (prod_count==0 && kfifo_is_empty(&cbuffer)) {up(&sem_mtx); return 0;}
 
-    kfifo_out(&cbuffer,kbuffer,len);
+    bytes_extracted = kfifo_out(&cbuffer,kbuffer,len);
 
-    cond_signal(prod);
+    if (nr_prod_waiting>0) {
+        up(&sem_prod);
+        nr_prod_waiting--;
+    }
     up(&sem_mtx);
 
-    if (copy_to_user(buff,kbuffer,len)) { return Error;}
-    return len;
+    if (bytes_extracted!=sizeof(int))
+        return -EINVAL;
+    
+    nr_bytes = sprintf(kbuffer, "%i\n", val);
+
+    if(len < nr_bytes)
+        return -ENOSPC;
+
+    if (copy_to_user(buff,kbuffer,nr_bytes)) { return -EFAULT;}
+
+    ((*off))+=nr_bytes;
+
+    return nr_bytes;
 }
 
-void fifoproc_release(bool lectura) {
+static int fifoproc_release(struct inode *inode, struct file *file) {
      if(down_interruptible(&sem_mtx))
         return -EINTR;
-    if(lectura==1){
+
+    if(file->f_mode & FMODE_READ){
         cons_count--;
-        cond_signal(prod);
+        
+        if (nr_prod_waiting>0) {
+            up(&sem_prod);
+            nr_prod_waiting--;
+        }
     }else{
         prod_count--;
-        cond_signal(cons);
+        if (nr_cons_waiting>0) {
+            up(&sem_cons);
+            nr_cons_waiting--;
+        }
     }
     kfifo_reset(&cbuffer);
     up(&sem_mtx);
+
+    return 0;
 }
+
+static const struct proc_ops proc_entry_fops =
+{
+    .proc_read = fifoproc_read,
+    .proc_write = fifoproc_write,
+    .proc_open = fifoproc_open,
+    .proc_release = fifoproc_release
+};
+
+int fifo_init_module(void){
+    int ret = 0;
+
+    proc_entry = proc_create( "fifoproc", 0666, NULL, &proc_entry_fops);
+
+    if (proc_entry == NULL) {
+        ret = -ENOMEM;
+        printk(KERN_INFO ">>> FIFOPROC: ERROR! Can't create /proc entry\n");
+    } else {
+        printk(KERN_INFO ">>> FIFOPROC: Module loaded\n");
+    }
+
+    ret = kfifo_alloc(&cbuffer, MAX_CBUFFER_LEN, GFP_KERNEL);
+    if(ret)
+        return -ENOMEM;
+
+    sema_init(&sem_prod,0);
+	sema_init(&sem_cons,0);
+
+	/* Inicializacion a 1 del semáforo que permite acceso en exclusión mutua a la SC */
+	sema_init(&sem_mtx,1);
+
+	printk(KERN_INFO "fifoproc: Cargado el Modulo.\n");
+
+	return ret;
+}
+
+
+void fifo_cleanup_module( void )
+{
+	remove_proc_entry("fifoproc", NULL);
+	kfifo_free(&cbuffer);
+	printk(KERN_INFO "fifoproc: Modulo descargado.\n");
+}
+
+
+module_init( fifo_init_module );
+module_exit( fifo_cleanup_module );
+
 
 
